dd556b598b5b0600a85531700eac6dd1eaf1c139
Adhith Ajayan <adithajayan3@gmail.com>
2026-03-19 11:51:42 +0530

    Force fix for missing database columns

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+python manage.py migrate staff zero --fake
+
+
+python manage.py migrate staff
+
+
 python manage.py migrate --fake-initial
+
+
 python manage.py collectstatic --noinput
 gunicorn config.wsgi:application
