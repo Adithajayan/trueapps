51f41df2886d0a498d418023ddb767f61a411782
Adhith Ajayan <adithajayan3@gmail.com>
2026-03-19 16:20:46 +0530

    Added ActivityLog model

--- job_management/models.py
+++ job_management/models.py
@@ -58,3 +58,12 @@ class Reminder(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ActivityLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.action} - {self.timestamp.strftime('%d-%m-%Y %H:%M')}"
