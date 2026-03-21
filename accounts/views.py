fd2f84a382981418a7a8796c9a277cb0ca6acd36
Adhith Ajayan <adithajayan3@gmail.com>
2026-03-21 16:57:34 +0530

    Dashboard fix: Remove ghost data when ExpenseType is deleted

--- accounts/views.py
+++ accounts/views.py
@@ -124,7 +124,7 @@ def dashboard(request):
     # ================= EXPENSE =================
     month_expense_total = Expense.objects.filter(
         date__range=[from_date, to_date],
-        category__isnull=False
+        expense_type__isnull=False
     ).aggregate(total=Sum('amount'))['total'] or Decimal('0')
 
     # ================= PROFIT STATUS =================
